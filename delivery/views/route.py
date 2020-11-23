2c22ff73394c5b9cd371027e74db7fbd85a11eae
Luiz Filipe <moresco.luiz@gmail.com>
2020-11-23 17:42:47 -0300

    Fix create method on view/Route

--- delivery/views/route.py
+++ delivery/views/route.py
@@ -4,8 +4,11 @@ from delivery.models import Route as RouteModel
 class Route:
     @staticmethod
     def create(route, db):
+        unique_route = route.dict().copy()
+        del unique_route['distance']
+
         exists = db.query(
-            db.query(RouteModel).filter_by(**route.dict()).exists(),
+            db.query(RouteModel).filter_by(**unique_route).exists(),
         ).scalar()
 
         if exists:
