788898f0d03ff29407cbe9c480ceecfdb3ded49b
Luiz Filipe <moresco.luiz@gmail.com>
2020-11-23 17:45:33 -0300

    Implement validate_route_exists method on views/Route

--- delivery/views/route.py
+++ delivery/views/route.py
@@ -4,14 +4,9 @@ from delivery.models import Route as RouteModel
 class Route:
     @staticmethod
     def create(route, db):
-        unique_route = route.dict().copy()
-        del unique_route['distance']
+        exists = Route.validate_route_exists(route)
 
-        exists = db.query(
-            db.query(RouteModel).filter_by(**unique_route).exists(),
-        ).scalar()
-
-        if exists:
+        if valid_input:
             raise ValueError('This route already exists.')
 
         new_route = RouteModel(**route.dict())
@@ -19,3 +14,13 @@ class Route:
         db.commit()
 
         return route
+
+    @staticmethod
+    def validate_route_exists(route):
+        unique_route = route.dict().copy()
+        del unique_route['distance']
+
+        return db.query(
+            db.query(RouteModel).filter_by(**unique_route).exists(),
+        ).scalar()
+
