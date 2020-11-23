9685944eeb16c449ba6884d711c83d3bdfbd78c3
Luiz Filipe <moresco.luiz@gmail.com>
2020-11-23 17:02:36 -0300

    Expose POST /route route

--- delivery/api.py
+++ delivery/api.py
@@ -2,9 +2,11 @@ import logging
 
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
+from sqlalchemy.orm.exc import NoResultFound
 
 from db import async_session
-from delivery.views import Healthcheck
+from delivery.views import Healthcheck, Route
+from delivery.schemas import RouteSchema
 
 
 router = APIRouter()
@@ -21,4 +23,23 @@ def healthcheck(db: Session = Depends(async_session)):
         raise HTTPException(
             status_code=503,
             detail='I am not live! :(',
+        )
+
+
+@router.post("/route", status_code=201, response_model=RouteSchema, tags=["Routes"])
+def new_route(route: RouteSchema, db: Session = Depends(async_session)):
+    try:
+        new_route = Route.create(route, db)
+        return new_route
+    except ValueError as e:
+        logger.error(e)
+        raise HTTPException(
+            status_code=400,
+            detail=str(e),
+        )
+    except Exception as e:
+        logger.error(e)
+        raise HTTPException(
+            status_code=400,
+            detail="Error to create a new route.",
         )
