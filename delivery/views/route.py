685ce9885c0c4595e355903839d1c1d070bcc744
Luiz Filipe <moresco.luiz@gmail.com>
2020-11-23 17:32:16 -0300

    Verify if the route already exists before adding the record

--- delivery/views/route.py
+++ delivery/views/route.py
@@ -4,6 +4,13 @@ from delivery.models import Route as RouteModel
 class Route:
     @staticmethod
     def create(route, db):
+        exists = db.query(
+            db.query(RouteModel).filter_by(**route.dict()).exists(),
+        ).scalar()
+
+        if exists:
+            raise ValueError('This route already exists.')
+
         new_route = RouteModel(**route.dict())
         db.add(new_route)
         db.commit()
