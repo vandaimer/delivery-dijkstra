d693f7b4529fba93ba6b66cda1ef4adbe6b8b74f
Luiz Filipe <moresco.luiz@gmail.com>
2020-11-23 23:17:26 -0300

    Implement RouteCheapestSchema schema

--- delivery/schemas.py
+++ delivery/schemas.py
@@ -1,6 +1,14 @@
+from typing import List
+
+from pydantic import BaseModel
 from pydantic_sqlalchemy import sqlalchemy_to_pydantic
 
 from .models import Route
 
 
 RouteSchema = sqlalchemy_to_pydantic(Route, exclude={"id"})
+
+
+class RouteCheapestSchema(BaseModel):
+    route: List[str]
+    cost: float
