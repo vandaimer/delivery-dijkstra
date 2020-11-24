4f061c38fe58ee02b205b0360626ed1b20476951
Luiz Filipe <moresco.luiz@gmail.com>
2020-11-23 23:18:48 -0300

    Rename method from validate_route_exists to validate_input

--- delivery/views/route.py
+++ delivery/views/route.py
@@ -4,7 +4,7 @@ from delivery.models import Route as RouteModel
 class Route:
     @staticmethod
     def create(route, db):
-        exists = Route.validate_route_exists(route)
+        valid_input = Route.validate_input(route, db)
 
         if valid_input:
             raise ValueError('This route already exists.')
@@ -16,11 +16,10 @@ class Route:
         return route
 
     @staticmethod
-    def validate_route_exists(route):
-        unique_route = route.dict().copy()
+    def validate_input(route, db):
+        unique_route = route.dict()
         del unique_route['distance']
 
         return db.query(
             db.query(RouteModel).filter_by(**unique_route).exists(),
         ).scalar()
-
