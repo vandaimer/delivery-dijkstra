3448725f8aa096671b32bc7ee89da10acca6375b
Luiz Filipe <moresco.luiz@gmail.com>
2020-11-24 00:16:03 -0300

    Implement TestHealthcheck

---
+++ delivery/views/tests/test_healthcheck.py
@@ -0,0 +1,15 @@
+from alchemy_mock.mocking import AlchemyMagicMock
+
+from delivery.views import Healthcheck
+
+
+class TestHealthcheck:
+
+    def test_status_OK(self, mocker):
+        session = AlchemyMagicMock()
+        expected = {'status': 'OK'}
+
+        response = Healthcheck.status(session)
+
+        session.execute.assert_called_once_with("SELECT 1")
+        assert response == expected
